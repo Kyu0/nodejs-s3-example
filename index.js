8bd978b4b26f99129ac8a44c55f414327b72fe23
Kyu0 <efftar@naver.com>
2024-03-14 23:29:26 +0900

    modified front files name index -> main (html, js)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const APP = express();
 const PORT = 5000;
 
 APP.get('/', (req, res) => {
-    fs.readFile('index.html', (error, data) => {
+    fs.readFile('main.html', (error, data) => {
         res.writeHead(200, { 'Content-Type' : 'text/html' });
         res.end(data);
     });
