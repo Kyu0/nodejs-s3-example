9a871840093cd7ebfa2ea4cbd4acc9b352e92ac6
Kyu0 <efftar@naver.com>
2024-03-14 23:57:50 +0900

    dependency added express-fileupload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fileupload from 'express-fileupload';
 import fs from 'fs';
 import path from 'path';
 
@@ -8,13 +9,20 @@ const APP = express();
 const PORT = 5000;
 
 APP.use('/script', express.static(__dirname + '/script'));
+APP.use(express.json());
+APP.use(fileupload());
+APP.use(express.urlencoded({ extended: true }));
 
 APP.get('/', (req, res) => {
     fs.readFile('main.html', (error, data) => {
         res.writeHead(200, { 'Content-Type' : 'text/html' });
         res.end(data);
     });
-});;
+});
+
+APP.post('/upload', (req, res) => {
+    console.log(req.files);
+});
 
 APP.listen(PORT, () => {
     console.log(`App running on port ${PORT}.`);
