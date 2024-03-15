774a45128f09fa24480c66ce6384eecdc93204e0
Kyu0 <efftar@naver.com>
2024-03-16 00:52:16 +0900

    completed

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ import express from 'express';
 import fileupload from 'express-fileupload';
 import fs from 'fs';
 import path from 'path';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
 const __dirname = path.resolve();
 
 const APP = express();
 const PORT = 5000;
+
+const S3 = new S3Client({
+    region: 'ap-northeast-2',
+    credentials: {
+        accessKeyId: '',
+        secretAccessKey: ''
+    }
+});
 
 APP.use('/script', express.static(__dirname + '/script'));
 APP.use('/upload', express.static(__dirname + '/upload'));
@@ -21,17 +30,31 @@ APP.get('/', (req, res) => {
     });
 });
 
-APP.post('/upload', (req, res) => {
+APP.post('/upload', async (req, res) => {
     let file = req.files.upload;
 
-    file.mv(`${__dirname}/upload/${file.name}`, (error) => {
-        if (error) {
-            return res.status(500).send(error);
-        }
-
-        res.json({ success: true, path: `/upload/${file.name}` });
+    const command = new PutObjectCommand({
+        Bucket: 'kyu0-test',
+        Key: 'public/image/sample.jpg', // 경로+파일명
+        Body: file.data
     });
+
+    try {
+        const response = await S3.send(command);
+        if (response.$metadata.httpStatusCode == 200) {
+            res.send({success: true, path: getSavedPath('public/image/sample.jpg')});
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ success: false, data: error});
+    }
 });
+
+function getSavedPath(filePath) {
+    const bucketname = 'kyu0-test';
+    const region = 'ap-northeast-2';
+    return `https://${bucketname}.s3.${region}.amazonaws.com/${filePath}`;
+}
 
 APP.listen(PORT, () => {
     console.log(`App running on port ${PORT}.`);
